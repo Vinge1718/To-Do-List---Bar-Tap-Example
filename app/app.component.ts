f145d5a01cb3f215cec3bd10f58bd9047dd79fc4
Victor King'e <vinge1718@gmail.com>
2017-09-23 12:03:33 +0300

    add the component annotation and the respetive component class to implement the design of the bar roo mtap and to-do list

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1 class="jumbotron">JS Angular Intro</h1>
+    <hr>
+    <h2 class="jumbotron">Tap Room App</h2>
+    <div class="row">
+      <h3>Keg brands available</h3>
+      <div class="col-md-6">
+        <button>Add Brand to the List</button><hr>
+      </div>
+      <div class="col-md-6"></div>
+    </div>
+  </div>
   `
 })
 
