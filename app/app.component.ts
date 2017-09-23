b6f4c24f55931e792401d630e54c3759bb2399a3
Victor King'e <vinge1718@gmail.com>
2017-09-23 16:21:33 +0300

    add the beer amount display and modifier

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,22 +13,31 @@ import { Component } from "@angular/core";
         <div *ngFor="let allTasks of parentTasks" id="tasksDisplay">
           <p>Task Description: {{allTasks.description}}</p>
           <p>Priority Level: {{allTasks.priority}}</p>
-          <p>Task categorisation: {{allTasks.category}}</p><hr>
+          <p>Task categorisation: {{allTasks.category}}</p>
+          <button (click)="editTask()">Change Task</button>
+          <hr>
         </div>
       </div>
     </div>
     <hr>
+
+
     <h2 class="jumbotron">Tap Room App</h2>
     <div class="row">
       <h3>Keg brands available</h3>
       <div class="col-md-6">
-        <button>Add Brand to the List</button><hr>
+        <button (click)="addKegToTaps()">Add Brand to the List</button><hr>
         <h4>Liqour on Tap!</h4>
         <div id="kegDisplay" *ngFor="let currentKeg of kegsOnTap">
           <p>Keg name: {{currentKeg.name}}</p>
           <p> Brew Brand: {{currentKeg.brand}}</p>
-          <p>Price per pint: {{currentKeg.price}}</p>
-          <p>Alcohol Content: {{currentKeg.alcohol}}%</p><hr>
+          <p>Price per pint: Ksh.{{currentKeg.price}}</p>
+          <p>Alcohol Content: {{currentKeg.alcohol}}%</p>
+          <p>Remaining pints: {{currentKeg.pintsContained}}</p>
+          <input type="number" min="1" id="pouredBeer">
+          <button (click)="getBeer(currentKeg)">Pour Some Beer</button><br>
+          <button (click)="editCurrentDrinks()">Edit Drink Details</button>
+          <hr>
         </div>
       </div>
       <div class="col-md-6"></div>
@@ -39,9 +48,9 @@ import { Component } from "@angular/core";
 
 export class AppComponent {
   public kegsOnTap: Keg[] = [
-    new Keg("Zcolsh", "EABL", 600, 4.8),
-    new Keg("Kifabisch", "Keroche", 750, 6.5),
-    new Keg("Nzstip", "Brew Bistro", 450, 6.5),
+    new Keg("Zcolsh", "EBAL", 600, 4.8),
+    new Keg("Kifabisch", "Kero", 750, 6.5),
+    new Keg("Nzstip", "BrewsBas", 450, 6.5),
     new Keg("lamstout", "DTP", 300, 5.2)
   ];
   public parentTasks: Task[] = [
@@ -55,6 +64,15 @@ export class AppComponent {
     new Task("Play video Game","Low","Hobby"),
     new Task("Cook Food","High","Home")
   ];
+  editTask() {
+
+  }
+  editCurrentDrinks() {
+
+  }
+  getBeer(pourBeerClicked: Task) {
+
+  }
 }
 
 export class Keg {
