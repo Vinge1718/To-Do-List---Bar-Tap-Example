c85037bf287234a3b2e950a904cdfc50c4959bb5
Victor King'e <vinge1718@gmail.com>
2017-09-24 20:35:06 +0300

    added the variables and angular decorators responsible for the sending the new task to the parent component

--- app/new-task.component.ts
+++ app/new-task.component.ts
@@ -4,10 +4,20 @@ import { Task } from "./task.model";
 @Component({
   selector: "new-task",
   template:`
-
+  <div class="col-md-4">
+    <h4>Create a New Task</h4>
+    <p>Task Description: <input #newDescription></p>
+    <p>Task Priority: <input #newPriority></p>
+    <p>Task Category: <input #newCategory></p>
+    <button (click)="addTaskToList(newDescription.value, newPriority.value, newCategory.value)">Add Task!</button>
+  </div>
   `
 })
 
 export class NewTaskComponent{
-
+  @Output() newTaskToSend = new EventEmitter();
+  addTaskToList(description: string, priority: string, category:string ){
+    var newTaskToAdd: Task = new Task(description, priority, category);
+    this.newTaskToSend.emit(newTaskToAdd);
+  }
 }
