360473bf6b0f4ea25846b130e571a6be6810ae06
Victor King'e <vinge1718@gmail.com>
2017-09-24 18:15:08 +0300

    provide a source for keg strucure manipulation

---
+++ app/keg.model.ts
@@ -0,0 +1,6 @@
+
+export class Keg {
+  public pintsContained: number = 40;
+  public value: boolean;
+  constructor(public name: string, public brand: string, public price: number, public alcohol: number){}
+}
