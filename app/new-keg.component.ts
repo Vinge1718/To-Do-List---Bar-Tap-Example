2649b59ebb1cab10352ba5d40364b17bfa31344a
Victor King'e <vinge1718@gmail.com>
2017-09-24 19:00:39 +0300

    add a component to add keg taps to the line up

---
+++ app/new-keg.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+import { Keg } from "./keg.model";
+
+@Component({
+  selector:"new-keg",
+  template:`
+
+  `
+})
+
+export class NewKegComponent{
+  
+}
