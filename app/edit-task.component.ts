bc98933efbfcfdfde0dc8f0d7747d3a5b3888787
Victor King'e <vinge1718@gmail.com>
2017-09-24 18:11:53 +0300

    add the child file for keg edits

---
+++ app/edit-task.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core"
+import { Task } from "./task.model"
+
+@Component ({
+  selector: "edit-task",
+  template: `
+  <div class="col-md-6" id="drinksEditor" *ngIf="childClickedTask">
+    <h4>Task Editor</h4>
+    <p>New Description: <input [(ngModel)]="childClickedTask.description"></p>
+    <p>Priority Level: <input [(ngModel)]="childClickedTask.priority"></p>
+    <p>Task Category: <input [(ngModel)]="childClickedTask.category"></p>
+    <button (click)="doneEdittingChildTasks()">save</button>
+  </div>
+  `
+})
+
+export class EditTaskComponent{
+@Input() childClickedTask: Task;
+@Output() doneEdittingChildTaskSender = new EventEmitter();
+doneEdittingChildTasks(){
+  this.doneEdittingChildTaskSender.emit();
+}
+}
