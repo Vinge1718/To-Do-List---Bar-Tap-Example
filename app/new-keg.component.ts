e89db5f81fdcfa6dc8c8ce27cd5288094468acd2
Victor King'e <vinge1718@gmail.com>
2017-09-24 20:34:08 +0300

    added the parts responsible for the sending the new task to the parent component

--- app/new-keg.component.ts
+++ app/new-keg.component.ts
@@ -2,12 +2,20 @@ import { Component, Output, EventEmitter } from "@angular/core";
 import { Keg } from "./keg.model";
 
 @Component({
-  selector:"new-keg",
+  selector:"new-tap",
   template:`
-
+  <div class="col-md-4">
+    <h4> Enter New Tap Details</h4>
+    <p>Enter Keg Name: <input></p>
+    <p>Brand Dealer: <input></p>
+    <p>Price per Pint: <input></p>
+    <p>Alcohol Content:<input></p>
+    <button (click)="addKegToTaps()">Add Another Keg Tap!</button>
+    <hr>
+  </div>
   `
 })
 
 export class NewKegComponent{
-  
+
 }
