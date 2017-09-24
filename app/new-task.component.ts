82af9e1e6d241bb9fc54b848e36ca67006a61358
Victor King'e <vinge1718@gmail.com>
2017-09-24 19:01:22 +0300

    add a component to add new tasks to the list

---
+++ app/new-task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter} from "@angular/core";
+import { Task } from "./task.model";
+
+@Component({
+  selector: "new-task",
+  template:`
+
+  `
+})
+
+export class NewTaskComponent{
+
+}
