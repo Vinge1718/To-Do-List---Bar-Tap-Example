0dbcaec4b76f4e6fceca3cccb2b4ad17d0d88557
Victor King'e <vinge1718@gmail.com>
2017-09-26 00:03:50 +0300

    add the task filter  component file

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
-import { TaskListComponent } from "./task-list.component"
-import { KegListComponent } from "./keg-list.component"
-import { EditTaskComponent} from "./edit-task.component"
-import { EditKegComponent} from "./edit-keg.component"
-import { NewTaskComponent } from "./new-task.component"
-import { NewKegComponent } from "./new-keg.component"
-import { CompleteTaskComponent } from "./task-pipe.component"
-import { KegViewFilterComponent } from "./keg-pipe.component"
+import { TaskListComponent } from "./task-list.component";
+import { KegListComponent } from "./keg-list.component";
+import { EditTaskComponent} from "./edit-task.component";
+import { EditKegComponent} from "./edit-keg.component";
+import { NewTaskComponent } from "./new-task.component";
+import { NewKegComponent } from "./new-keg.component";
+import { CompleteTaskComponent } from "./task-pipe.component";
+import { KegViewFilterComponent } from "./keg-pipe.component";
+import { TaskCheckComponent } from "./task.component";
 
 @NgModule({
   imports: [
@@ -25,7 +26,8 @@ import { KegViewFilterComponent } from "./keg-pipe.component"
     NewKegComponent,
     NewTaskComponent,
     CompleteTaskComponent,
-    KegViewFilterComponent
+    KegViewFilterComponent,
+    TaskCheckComponent
    ],
   bootstrap: [AppComponent]
 })
