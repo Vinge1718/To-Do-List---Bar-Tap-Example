3b0fbb9775c1069d8464197905cae2f8ead70b6d
Victor King'e <vinge1718@gmail.com>
2017-09-26 02:55:49 +0300

    restore page stylinng

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -4,7 +4,7 @@ import { Keg } from "./keg.model"
  @Component({
    selector: "keg-list",
    template:`
-   <select (change)="uponSelection($event.target.value)">
+   <select (change)="uponSelection($event.target.value)" class="filter">
     <option value="all">All Available Liquour on Tap</option>
     <option value="A/C above 10%">Alcohol Content exceeding 10%</option>
     <option value="A/C below 10%">Alcohol Content below 10%</option>
@@ -17,8 +17,8 @@ import { Keg } from "./keg.model"
      <p>Price per pint: Ksh.{{currentKeg.price}}</p>
      <p>Alcohol Content: {{currentKeg.alcohol}}%</p>
      <p>Remaining pints: {{currentKeg.pintsContained}}</p>
-     <input type="number" min="1" id="pouredBeer">
-     <button (click)="getBeer()">Pour Some Beer</button><br><br>
+     <input type="number" min="1" id="pouredBeer" #amountPoured.value>
+     <button (click)="getBeer(amountPoured.value); amountPoured.value='';">Pour Some Beer</button><br><br>
      <button (click)="editCurrentDrinks(currentKeg)">Edit Drink Details</button>
      <hr>
    </div>
@@ -30,6 +30,12 @@ import { Keg } from "./keg.model"
     @Output() kegClickSender = new EventEmitter();
     editCurrentDrinks(clickedKeg: Keg){
       this.kegClickSender.emit(clickedKeg);
+  }
+
+
+  getBeer(pintsPoured: number){
+  var beerPoured: number = pintsPoured;
+  
   }
 
   public selectedView: string = "all";
